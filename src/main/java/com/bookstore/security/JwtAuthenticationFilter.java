4642dd1a6b940b4b9148b5e069000f2a0b467f9e
Roman K <romazan07sq@gmail.com>
2025-06-11 21:19:54 +0300

    fix: minor corrections

--- src/main/java/com/bookstore/security/JwtAuthenticationFilter.java
+++ src/main/java/com/bookstore/security/JwtAuthenticationFilter.java
@@ -19,7 +19,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-    private static final String BEARER_PREFIX = "bearer ";
+    private static final String BEARER_PREFIX = "Bearer ";
     private final JwtUtil jwtUtil;
     private final UserDetailsService userDetailsService;
 
